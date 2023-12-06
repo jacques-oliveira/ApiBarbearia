b71209937d5c0f2c573fe408f9a6a9afe0d669e6
jacques-oliveira <jacques.oliveira.eng@gmail.com>
2023-12-05 21:53:57 -0300

    ProdutoDTO

---
+++ DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+
+namespace ApiBarbearia.DTOs.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Produto, ProdutoDTO>().ReverseMap();
+            CreateMap<Categoria, CategoriaDTO>().ReverseMap();
+        }
+    }
+}
---
+++ DTOs/ProdutoDTO.cs
@@ -0,0 +1,8 @@
+public class ProdutoDTO{   
+    public int ProdutoId { get; set; }
+    public string? Nome {get; set;}
+    public string? Descricao {get; set;}
+    public decimal Preco {get; set;}
+    public string? ImagemUrl {get; set;}
+    public int CategoriaId {get; set;}     
+}
