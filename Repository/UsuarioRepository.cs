274b6def68886b6b6160dd9189671f7d818e06ae
Jacques <130871640+jacques-oliveira@users.noreply.github.com>
2024-04-14 21:33:02 -0300

    correção retorno dados usuário

--- Repository/UsuarioRepository.cs
+++ Repository/UsuarioRepository.cs
@@ -9,7 +9,10 @@ public class UsuarioRepository : Repository<Usuario>, IUsuarioRepository
 
     public async Task<IEnumerable<Usuario>> GetDadosUsuarios()
     {
-        return await Get().Include(e=> e.Endereco).ToListAsync();
+        return await Get()
+                    .Include(e=> e.Endereco).Where(e=> e.Endereco != null)
+                    .Include(em=> em.Email).Where(em=> em.Email != null)
+                    .ToListAsync();
     }
 
     public async Task<PagedList<Usuario>> GetUsuarios(UsuariosParameters usuariosParameters)
