140f13776c99c76e0d7e08c3320b49673963f4cb
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-03-27 16:12:10 -0300

    alterando nome para email

--- DTOs/LoginModelDTO.cs
+++ DTOs/LoginModelDTO.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 public class LoginModelDTO {
     [Required(ErrorMessage ="Nome de usuário obriagtório")]
-    public string? UserName { get; set; }
+    public string? Email { get; set; }
 
     [Required(ErrorMessage ="Password obrigatório")]
     public string? Password {get; set; }
